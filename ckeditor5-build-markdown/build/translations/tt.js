8af491921995643fd502d9dac30c5a1c9468c02d
Adam Rose <adrotog@gmail.com>
2022-01-05 18:48:17 -0500

    Remove translation files

--- ckeditor5-build-markdown/build/translations/tt.js
+++
@@ -1,1 +0,0 @@
-!function(t){const n=t.tt=t.tt||{};n.dictionary=Object.assign(n.dictionary||{},{"%0 of %1":"",Bold:"Калын",Cancel:"",Italic:"",Redo:"Кабатла",Save:"Сакла","Show more items":"",Strikethrough:"",Underline:"",Undo:""}),n.getPluralForm=function(t){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
